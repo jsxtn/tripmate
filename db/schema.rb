dffa8a089ba3b8aa7c6c9ad1df1ebd7f5b522daa
Joshua Sexton <joshua.peter.sexton+6RIW@gmail.com>
2024-05-14 17:59:28 +0100

    pushing icon and logo pngs, bootstrap class changes to brand colours and also a custom gradient css partial for split primary colours from tripmate brand orange

--- db/schema.rb
+++ db/schema.rb
@@ -45,9 +45,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_163827) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "first_name"
-    t.string "last_name"
-    t.text "preferences"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
