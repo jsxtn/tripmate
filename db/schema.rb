7ed0b189b5a70af8f64e8b1ae62c7b002c994927
Tony Nour <tonyanour1@gmail.com>
2024-05-14 17:35:33 +0100

    added 3 new columns to activity table and created seed file for 3 users, 3 itineraries and 87 activities in total

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_13_163827) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_14_132248) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,9 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_13_163827) do
     t.bigint "itinerary_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "phone_number"
+    t.text "url"
     t.index ["itinerary_id"], name: "index_activities_on_itinerary_id"
   end
 
