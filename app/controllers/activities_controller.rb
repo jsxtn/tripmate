1ec05cd2e2d5d30d1d463666020fed0612e8bdad
Tony Nour <tonyanour1@gmail.com>
2024-05-15 15:00:15 +0100

    core flow built into controllers and views

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,2 +1,24 @@
 class ActivitiesController < ApplicationController
+
+
+  def index
+    # @itineraries = Itinerary.all
+    @username = current_user.first_name
+
+    # @itineraries = Itinerary.find(user_id: current_user.id)
+    @itinerary = Itinerary.find(params[:itinerary_id])
+    @activities_by_datetime = @itinerary.activities.group_by {|activity| activity.datetime.to_date}.values
+
+  end
+
+
+  def show
+    # @itineraries = Itinerary.all
+    @username = current_user.first_name
+    # @itinerary = Itinerary.find(params[:itinerary_id])
+    @activity = Activity.find(params[:id])
+    @itinerary = Itinerary.find(@activity.itinerary_id)
+
+  end
+
 end
--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -1,2 +1,11 @@
 class ItinerariesController < ApplicationController
+
+  def index
+    # @itineraries = Itinerary.all
+    @username = current_user.first_name
+
+    # @itineraries = Itinerary.find(user_id: current_user.id)
+    @itineraries = Itinerary.where(user_id: current_user.id)
+
+  end
 end
