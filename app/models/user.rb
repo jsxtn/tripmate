06e956131e4985b1eec348c6bc554b887f16db53
Tony Nour <tonyanour1@gmail.com>
2024-05-21 15:14:13 +0100

    fixed map

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :itineraries, dependent: :destroy
+  has_many :activities, through: :itineraries
+
+
 end
