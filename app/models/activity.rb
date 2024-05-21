c5cde7911ce86929639ea10a34b308927c834e94
Hafsah Chughtai <hafsahc2002@outlook.com>
2024-05-21 10:25:06 +0100

    created maps

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,9 @@
 class Activity < ApplicationRecord
   belongs_to :itinerary
   validates :datetime, :longitude, :latitude, :title, :category, presence: true
+
+  # geocoding
+  geocoded_by :address
+  after_validation :geocode, if:
+   :will_save_change_to_address?
 end
