ab7524664e9aff7a7f83beb84878cdef8427732e
Tony Nour <tonyanour1@gmail.com>
2024-05-23 14:59:12 +0100

    fixed chat page and few others

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,13 +2,15 @@ import { Controller } from "@hotwired/stimulus"
 import flatpickr from "flatpickr"; // You need to import this to use new flatpickr()
 
 export default class extends Controller {
+  static values = { strings: Array }
   connect() {
+    console.log(this.stringsValue)
     flatpickr(this.element
-  //     , {
+       , {
   //     mode: "range",
-  //     minDate: "today",
-  //     dateFormat: "d-m-Y"
-  // }
+       minDate: "today",
+       dateFormat: "d-m-Y"
+   }
   )
   }
 }
