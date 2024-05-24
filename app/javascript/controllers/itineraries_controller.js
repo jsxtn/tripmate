06a6df8d480a03b83eef0055beced554e39e3c9c
Roel de Jong <Roel4811@hotmail.com>
2024-05-24 12:15:18 +0200

    find closing div

--- app/javascript/controllers/itineraries_controller.js
+++ app/javascript/controllers/itineraries_controller.js
@@ -7,11 +7,13 @@ export default class extends Controller {
     console.log(mapButton)
     mapButton.addEventListener('click', function() {
       if (elementsHidden) {
+        console.log('fading in all elements')
         // fade in all elements
-        $('body').children().not('.c2a-button-map-itin, .no-show-itin, #navbar-itin, #map-act').fadeIn();
+        $('body').children().not('.c2a-button-map-itin, #navbar-itin, #map-act').fadeIn();
       } else {
+        console.log('fading out all elements except specified ones')
         // fade out all elements except the specified ones
-        $('body').children().not('.c2a-button-map-itin, .no-show-itin, #navbar-itin, #map-act').fadeOut();
+        $('body').children().not('.c2a-button-map-itin, #navbar-itin, #map-act').fadeOut();
       }
       elementsHidden = !elementsHidden;
     });
